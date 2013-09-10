b622046b145ff1188f2ce3d2b384686b35a0e181
Max Barkley <mbarkley@redhat.com>
2013-09-10 16:33:44 -0400

    Cleaned up annotation matcher

--- errai-ioc/src/main/java/org/jboss/errai/ioc/rebind/ioc/metadata/JSR330QualifyingMetadata.java
+++ errai-ioc/src/main/java/org/jboss/errai/ioc/rebind/ioc/metadata/JSR330QualifyingMetadata.java
@@ -85,20 +85,9 @@ public class JSR330QualifyingMetadata implements QualifyingMetadata {
   }
   
   private static void addQualifiersForSource(final Set<Annotation> addTo, final Set<Annotation> from) {
-    boolean hasNew = false, hasAny = false;
-    
     for (Annotation a : from) {
-      if (a.annotationType().equals(New.class)) {
-        hasNew = true;
-        continue;
-      }
-      else if (a.annotationType().equals(Any.class))
-        hasAny = true;
-      addTo.add(a);
-    }
-    
-    if (!hasAny && !hasNew) {
-      addTo.add(BuiltInQualifiers.ANY_INSTANCE);
+      if (!a.annotationType().equals(New.class))
+        addTo.add(a);
     }
   }
 
