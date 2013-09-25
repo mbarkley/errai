a0c61e09e993ac1b652296347cff1da4365e4cd5
Max Barkley <mbarkley@redhat.com>
2013-09-25 14:09:55 -0400

    Refined CDI service annotation tests

--- errai-cdi/weld-integration/src/test/java/org/jboss/errai/cdi/event/server/ClassWithServiceAndCommandMethod.java
+++ errai-cdi/weld-integration/src/test/java/org/jboss/errai/cdi/event/server/ClassWithServiceAndCommandMethod.java
@@ -19,4 +19,10 @@ public class ClassWithServiceAndCommandMethod {
     MessageBuilder.createConversation(message).subjectProvided().noErrorHandling().sendNowWith(bus);
   }
   
+  @Service("ClassWithServiceAndCommandMethod")
+  @Command
+  private void badCommand(Message message) {
+    throw new RuntimeException("This should never be called!");
+  }
+  
 }
