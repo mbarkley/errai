a80ef95830787ad5dfee7eb513e0eeefa79f1891
Max Barkley <mbarkley@redhat.com>
2013-10-21 12:57:33 -0400

    Add unimplemented ServletContainerLauncher

---
+++ errai-cdi/jboss/src/main/java/org/jboss/errai/cdi/server/gwt/JBossLauncher.java
@@ -0,0 +1,18 @@
+package org.jboss.errai.cdi.server.gwt;
+
+import java.io.File;
+import java.net.BindException;
+
+import com.google.gwt.core.ext.ServletContainer;
+import com.google.gwt.core.ext.ServletContainerLauncher;
+import com.google.gwt.core.ext.TreeLogger;
+
+public class JBossLauncher extends ServletContainerLauncher {
+
+  @Override
+  public ServletContainer start(TreeLogger logger, int port, File appRootDir) throws BindException, Exception {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+}
