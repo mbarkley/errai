e2736d10123126b5d265fea17ba50418cf9bb17a
Max Barkley <mbarkley@redhat.com>
2013-10-24 11:37:06 -0400

    Switch to using external JBoss AS given by user through JVM property

--- errai-cdi/jboss/src/main/java/org/jboss/errai/cdi/server/gwt/JBossLauncher.java
+++ errai-cdi/jboss/src/main/java/org/jboss/errai/cdi/server/gwt/JBossLauncher.java
@@ -1,13 +1,9 @@
 package org.jboss.errai.cdi.server.gwt;
 
 import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
 import java.io.IOException;
 import java.net.BindException;
 import java.util.Stack;
-import java.util.zip.ZipEntry;
-import java.util.zip.ZipInputStream;
 
 import org.jboss.errai.cdi.server.as.JBossServletContainerAdaptor;
 
@@ -16,16 +12,12 @@ import com.google.gwt.core.ext.ServletContainerLauncher;
 import com.google.gwt.core.ext.TreeLogger;
 import com.google.gwt.core.ext.TreeLogger.Type;
 import com.google.gwt.core.ext.UnableToCompleteException;
-import com.google.gwt.thirdparty.guava.common.io.Files;
 
 public class JBossLauncher extends ServletContainerLauncher {
 
-  private final String JBOSS_ZIP_RESOURCE = "jboss-as-7.1.1.Final.zip";
-  // TODO make portable
+  private final String JBOSS_HOME_PROPERTY = "errai.jboss.home";
 
   private final Stack<TreeLogger> branches = new Stack<TreeLogger>();
-
-  private final int BUF_SIZE = 1024;
 
   @Override
   public ServletContainer start(TreeLogger logger, int port, File appRootDir) throws BindException, Exception {
@@ -33,11 +25,17 @@ public class JBossLauncher extends ServletContainerLauncher {
 
     branches.push(branches.peek().branch(Type.INFO, "Starting launcher..."));
 
-    branches.push(branches.peek().branch(Type.INFO, String.format("Unpacking %s...", JBOSS_ZIP_RESOURCE)));
-    final String JBOSS_HOME = unpack();
-    branches.pop().log(Type.INFO, String.format("Finished unpacking %s", JBOSS_ZIP_RESOURCE));
+    final String JBOSS_HOME = System.getProperty(JBOSS_HOME_PROPERTY);
+    if (JBOSS_HOME == null) {
+      branches.peek().log(
+              Type.ERROR,
+              String.format(
+                      "No value for %s was found: The root directory of your JBoss installation must given to the JVM",
+                      JBOSS_HOME_PROPERTY));
+      throw new UnableToCompleteException();
+    }
 
-    final String JBOSS_START = JBOSS_HOME + "/bin/standalone-debug.sh";
+    final String JBOSS_START = JBOSS_HOME + "/bin/" + getStartScriptName();
 
     Process process;
     try {
@@ -79,104 +77,8 @@ public class JBossLauncher extends ServletContainerLauncher {
     }
   }
 
-  // TODO clean up these files when done
-  private String unpack() throws UnableToCompleteException {
-    File tmpDir = Files.createTempDir();
-    branches.peek().log(Type.INFO, String.format("Created root directory %s", tmpDir.getAbsolutePath()));
-
-    branches.peek().log(Type.INFO, String.format("Opening stream for %s", JBOSS_ZIP_RESOURCE));
-    ZipInputStream in = new ZipInputStream(ClassLoader.getSystemResourceAsStream(JBOSS_ZIP_RESOURCE));
-    ZipEntry entry;
-    // Get first file/directory in zip
-    try {
-      entry = in.getNextEntry();
-    } catch (IOException e1) {
-      branches.peek().log(Type.ERROR, String.format("Could not unpack %s", JBOSS_ZIP_RESOURCE), e1);
-      throw new UnableToCompleteException();
-    }
-    while (entry != null) {
-      if (entry.isDirectory()){
-        final File file = new File(tmpDir, entry.getName());
-        if (!file.mkdirs()) {
-          branches.peek().log(Type.WARN, String.format("Failed to make directories for %s", file.getAbsolutePath()));
-        }
-      }
-      else {
-        final File file = new File(tmpDir, entry.getName());
-        if (!file.getParentFile().mkdirs()) {
-          branches.peek().log(Type.WARN, String.format("Failed to make directories for %s", file.getAbsolutePath()));
-        }
-        try {
-          branches.peek().log(Type.DEBUG, String.format("Creating %s...", file.getAbsolutePath()));
-          file.createNewFile();
-        } catch (IOException e1) {
-          branches.peek().log(Type.ERROR, String.format("Could not create file %s", file.getAbsolutePath()), e1);
-          throw new UnableToCompleteException();
-        }
-
-        // Create file writer
-        final FileOutputStream writer;
-        try {
-          writer = new FileOutputStream(file);
-        } catch (FileNotFoundException e1) {
-          branches.peek().log(Type.ERROR,
-                  String.format("Could not create FileOutputStream for %s", file.getAbsolutePath()), e1);
-          throw new UnableToCompleteException();
-        }
-
-        branches.peek().log(Type.DEBUG, String.format("Writing to %s...", file.getAbsolutePath()));
-        final byte[] buf = new byte[BUF_SIZE];
-        int len = 0;
-        while (len != -1) {
-          // Fill and write buffer
-          try {
-            len = in.read(buf);
-            if (len > 0) {
-              writer.write(buf, 0, len);
-            }
-          } catch (IOException e) {
-            branches.peek().log(Type.ERROR, String.format("Could not write to %s", file.getAbsolutePath()), e);
-            try {
-              writer.close();
-            } catch (IOException e1) {
-              branches.peek().log(Type.WARN, String.format("Could not close writer stream for %s", file.getName()), e1);
-            }
-            throw new UnableToCompleteException();
-          }
-        }
-
-        try {
-          writer.close();
-        } catch (IOException e) {
-          branches.pop().log(Type.WARN, String.format("Could not close writer stream for %s", file.getName()), e);
-        }
-      }
-      // Get next file/directory
-      try {
-        entry = in.getNextEntry();
-      } catch (IOException e1) {
-        branches.peek().log(Type.ERROR, String.format("Could not unpack %s", JBOSS_ZIP_RESOURCE), e1);
-        throw new UnableToCompleteException();
-      }
-    }
-
-    branches.push(branches.peek().branch(Type.INFO, "Fixing permissions..."));
-    fixPerms(tmpDir);
-
-    return tmpDir.getAbsolutePath() + File.separator + tmpDir.listFiles()[0].getName();
-  }
-
-  private void fixPerms(File file) {
-    branches.peek().log(Type.DEBUG, String.format("Setting permissions for %s", file.getAbsolutePath()));
-    file.setReadable(true);
-    file.setWritable(true);
-    file.setExecutable(true);
-
-    branches.peek().log(Type.DEBUG, String.format("Descending into %s...", file.getAbsolutePath()));
-    if (file.isDirectory()) {
-      for (File child : file.listFiles()) {
-        fixPerms(child);
-      }
-    }
+  // TODO make portable
+  private String getStartScriptName() {
+    return "standalone.sh";
   }
 }
