9ee717a03e8e74492ba43ea0eea9a435820b871e
Max Barkley <mbarkley@redhat.com>
2014-06-23 11:12:30 -0400

    Make LoginRedirect URL configurable in errai-security-keycloak.

--- errai-security/errai-security-keycloak/src/main/java/org/jboss/errai/security/keycloak/LoginRedirectFilter.java
+++ errai-security/errai-security-keycloak/src/main/java/org/jboss/errai/security/keycloak/LoginRedirectFilter.java
@@ -16,12 +16,23 @@ import javax.servlet.http.HttpServletResponse;
 import org.keycloak.KeycloakSecurityContext;
 
 /**
- * Redirects users to the Keycloak server for authentication.
+ * <p>
+ * Used for forcing Keycloak logins in applications that wish to allow unauthenticated access to the
+ * GWT host page. By adding this filter and a security constraint to a URL, a request to that URL
+ * will redirect to a Keycloak login. After a successful login, this filter will redirect to a
+ * path specified by the {@value #REDIRECT_PARAM_NAME} filter initParm.
+ *
+ * <p>
+ * The {@link #REDIRECT_PARAM_NAME} is relative to the application context.
  *
  * @author Max Barkley <mbarkley@redhat.com>
  */
 @WebFilter(filterName = "ErraiLoginRedirectFilter")
 public class LoginRedirectFilter implements Filter {
+
+  public static final String REDIRECT_PARAM_NAME = "redirectLocation";
+
+  private String redirectLocation;
 
   @Inject
   private ServletContext servletContext;
@@ -31,16 +42,20 @@ public class LoginRedirectFilter implements Filter {
 
   @Override
   public void init(final FilterConfig filterConfig) throws ServletException {
+    final String redirectParam = filterConfig.getInitParameter(REDIRECT_PARAM_NAME);
+    redirectLocation = servletContext.getContextPath();
+    if (redirectParam != null) {
+      redirectLocation += redirectParam;
+    }
   }
 
   @Override
   public void doFilter(final ServletRequest request, final ServletResponse response,
-      final FilterChain chain) throws IOException, ServletException {
+          final FilterChain chain) throws IOException, ServletException {
     final HttpServletResponse httpResponse = (HttpServletResponse) response;
     keycloakAuthService.setSecurityContext((KeycloakSecurityContext) request.getAttribute(KeycloakSecurityContext.class
             .getName()));
-    // TODO make this configurable
-    httpResponse.sendRedirect(servletContext.getContextPath());
+    httpResponse.sendRedirect(redirectLocation);
   }
 
   @Override
