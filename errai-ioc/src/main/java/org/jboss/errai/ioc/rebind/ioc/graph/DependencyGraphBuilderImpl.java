503823d23d16213f8273a89f54276acb96eec0ff
Max Barkley <mbarkley@redhat.com>
2015-08-11 12:02:44 -0400

    Add validation logic for dependencies.

--- errai-ioc/src/main/java/org/jboss/errai/ioc/rebind/ioc/graph/DependencyGraphBuilderImpl.java
+++ errai-ioc/src/main/java/org/jboss/errai/ioc/rebind/ioc/graph/DependencyGraphBuilderImpl.java
@@ -5,8 +5,12 @@ import java.util.Arrays;
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Map;
+import java.util.Queue;
 import java.util.Set;
+import java.util.Stack;
 
 import org.jboss.errai.codegen.meta.MetaClass;
 import org.jboss.errai.ioc.rebind.ioc.injector.api.WiringElementType;
@@ -96,6 +100,110 @@ public class DependencyGraphBuilderImpl implements DependencyGraphBuilder {
   @Override
   public DependencyGraph createGraph() {
     linkDependencyAliases();
+    validateDependencies();
+  }
+
+  private void validateDependencies() {
+    final Set<Concrete> visited = new HashSet<Concrete>();
+    final Stack<DFSFrame> visiting = new Stack<DFSFrame>();
+
+    for (final Concrete concrete : concretes) {
+      visiting.push(new DFSFrame(concrete));
+      do {
+        final DFSFrame curFrame = visiting.peek();
+        if (curFrame.dependencyIndex < curFrame.concrete.dependencies.size()) {
+          final Dependency dep = curFrame.concrete.dependencies.get(curFrame.dependencyIndex);
+          final Concrete resolved = resolveDependency(dep);
+          if (visited.contains(resolved)) {
+            if (visiting.contains(resolved)) {
+              validateCycle(visiting, resolved);
+            }
+          } else {
+            curFrame.dependencyIndex += 1;
+            visiting.push(new DFSFrame(resolved));
+          }
+        } else {
+          visiting.pop();
+        }
+      } while (visiting.size() > 0);
+    }
+  }
+
+  private void validateCycle(final Stack<DFSFrame> visiting, final Concrete resolved) {
+    if (canBreakCycle(resolved)) {
+      return;
+    }
+
+    int i;
+    for (i = visiting.size() - 1; !visiting.get(i).equals(resolved); i--) {
+      if (canBreakCycle(visiting.get(i).concrete)) {
+        return;
+      }
+    }
+
+    throwCycleErrorMessage(visiting, i);
+  }
+
+  private void throwCycleErrorMessage(final Stack<DFSFrame> visiting, final int startIndex) {
+    final StringBuilder messageBuilder = new StringBuilder();
+    messageBuilder.append("The following cycle cannot be wired because it contains no normal scoped beans:\n");
+    for (int i = startIndex; i < visiting.size(); i++) {
+      final MetaClass injectedType = visiting.get(i).concrete.getInjectedType();
+      messageBuilder.append(injectedType.getName())
+                    .append(" -> ");
+    }
+    messageBuilder.append(visiting.get(startIndex).concrete.getInjectedType().getName());
+
+    throw new RuntimeException(messageBuilder.toString());
+  }
+
+  private boolean canBreakCycle(final Concrete resolved) {
+    for (final WiringElementType wiringType : resolved.wiringTypes) {
+      if (WiringElementType.NormalScopedBean.equals(wiringType)) {
+        return true;
+      }
+    }
+
+    return false;
+  }
+
+  private Concrete resolveDependency(final Dependency dep) {
+    final List<Concrete> resolved = new ArrayList<Concrete>();
+    final Queue<Alias> resolutionQueue = new LinkedList<Alias>();
+    resolutionQueue.add(dep.alias);
+
+    do {
+      final Alias cur = resolutionQueue.poll();
+      for (final Entity link : cur.linked) {
+        if (link instanceof Alias) {
+          resolutionQueue.add((Alias) link);
+        } else if (link instanceof Concrete) {
+          resolved.add((Concrete) link);
+        }
+      }
+    } while (resolutionQueue.size() > 0);
+
+    if (resolved.isEmpty()) {
+      // TODO improve message
+      throw new RuntimeException("Unsatisfied dependency: " + dep.alias.type.getName());
+    } else if (resolved.size() > 1) {
+      throwAmbiguousDependencyException(dep, resolved);
+    }
+
+    return resolved.get(0);
+  }
+
+  private void throwAmbiguousDependencyException(final Dependency dep, final List<Concrete> resolved) {
+    final StringBuilder messageBuilder = new StringBuilder();
+    messageBuilder.append("Ambiguous resolution for type " + dep.alias.type.getName() + ".\n")
+                  .append("Resolved types:\n")
+                  .append(resolved.get(0));
+    for (int i = 1; i < resolved.size(); i++) {
+      messageBuilder.append(", ")
+                    .append(resolved.get(i).type.getName());
+    }
+
+    throw new RuntimeException(messageBuilder.toString());
   }
 
   private void linkDependencyAliases() {
@@ -145,7 +253,7 @@ public class DependencyGraphBuilderImpl implements DependencyGraphBuilder {
   static class Concrete extends Entity {
     final InjectorType injectorType;
     final Collection<WiringElementType> wiringTypes;
-    final Collection<Dependency> dependencies = new ArrayList<Dependency>();
+    final List<Dependency> dependencies = new ArrayList<Dependency>();
 
     Concrete(final MetaClass type, final Qualifier qualifier, final InjectorType injectorType, final Collection<WiringElementType> wiringTypes) {
       super(type, qualifier);
@@ -185,6 +293,29 @@ public class DependencyGraphBuilderImpl implements DependencyGraphBuilder {
     @Override
     public int hashCode() {
       return type.hashCode() ^ qualifier.hashCode();
+    }
+  }
+
+  static class DFSFrame {
+    final Concrete concrete;
+    int dependencyIndex = 0;
+
+    DFSFrame(final Concrete concrete) {
+      this.concrete = concrete;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+      if (obj instanceof Concrete) {
+        return concrete.equals(obj);
+      } else {
+        return super.equals(obj);
+      }
+    }
+
+    @Override
+    public int hashCode() {
+      return concrete.hashCode();
     }
   }
 
