d4c71712740351b07d2680a2a8691333a7aab9f5
Max Barkley <mbarkley@redhat.com>
2015-08-18 11:47:24 -0400

    Wrap annotations to avoid hashcode/equals problems with @Any and @Default.

--- errai-ioc/src/main/java/org/jboss/errai/ioc/rebind/ioc/graph/DefaultQualifierFactory.java
+++ errai-ioc/src/main/java/org/jboss/errai/ioc/rebind/ioc/graph/DefaultQualifierFactory.java
@@ -19,53 +19,74 @@ public class DefaultQualifierFactory implements QualifierFactory {
     public Class<? extends Annotation> annotationType() {
       return Any.class;
     }
+    @Override
+    public String toString() {
+      return "@Any";
+    }
+    @Override
+    public boolean equals(Object obj) {
+      return obj instanceof Any;
+    }
   };
+
+  private static final AnnotationWrapper ANY_WRAPPER = new AnnotationWrapper(ANY);
 
   private static final Default DEFAULT = new Default() {
     @Override
     public Class<? extends Annotation> annotationType() {
       return Default.class;
     }
+    @Override
+    public String toString() {
+      return "@Default";
+    }
+    @Override
+    public boolean equals(Object obj) {
+      return obj instanceof Default;
+    }
   };
+
+  private static final AnnotationWrapper DEFAULT_WRAPPER = new AnnotationWrapper(DEFAULT);
 
   private static final Qualifier UNIVERSAL = new Universal();
 
-  private final Map<Set<Annotation>, NormalQualifier> qualifiers = new HashMap<Set<Annotation>, NormalQualifier>();
+  private final Map<Set<AnnotationWrapper>, NormalQualifier> qualifiers = new HashMap<Set<AnnotationWrapper>, NormalQualifier>();
 
   {
-    qualifiers.put(Collections.<Annotation>emptySet(), new NormalQualifier(Collections.<Annotation>emptySet()));
+    qualifiers.put(Collections.<AnnotationWrapper>emptySet(), new NormalQualifier(Collections.<AnnotationWrapper>emptySet()));
   }
 
   @Override
   public Qualifier forConcreteInjectable(final HasAnnotations annotated) {
-    final Set<Annotation> annos = getQualifierAnnotations(annotated);
-    annos.add(ANY);
+    final Set<AnnotationWrapper> annos = getQualifierAnnotations(annotated);
+    annos.add(ANY_WRAPPER);
 
     return getOrCreateQualifier(annos);
   }
 
   @Override
   public Qualifier forAbstractInjectable(final HasAnnotations annotated) {
-    final Set<Annotation> annos = getQualifierAnnotations(annotated);
+    final Set<AnnotationWrapper> annos = getQualifierAnnotations(annotated);
 
     return getOrCreateQualifier(annos);
   }
 
-  private NormalQualifier getOrCreateQualifier(final Set<Annotation> annos) {
+  private NormalQualifier getOrCreateQualifier(final Set<AnnotationWrapper> annos) {
     NormalQualifier qualifier = qualifiers.get(annos);
     if (qualifier == null) {
       qualifier = new NormalQualifier(annos);
       qualifiers.put(annos, qualifier);
     }
+
     return qualifier;
   }
 
-  private Set<Annotation> getQualifierAnnotations(final HasAnnotations annotated) {
-    final Set<Annotation> annos = new HashSet<Annotation>();
+  private Set<AnnotationWrapper> getQualifierAnnotations(final HasAnnotations annotated) {
+    final Set<AnnotationWrapper> annos = new HashSet<AnnotationWrapper>();
     for (final Annotation anno : annotated.getAnnotations()) {
       // TODO handle stereotypes
       if (anno.annotationType().isAnnotationPresent(javax.inject.Qualifier.class)) {
-        annos.add(anno);
+        annos.add(new AnnotationWrapper(anno));
       }
     }
     addDefaulIfUnqualified(annos);
@@ -73,9 +94,9 @@ public class DefaultQualifierFactory implements QualifierFactory {
     return annos;
   }
 
-  private void addDefaulIfUnqualified(final Set<Annotation> annos) {
+  private void addDefaulIfUnqualified(final Set<AnnotationWrapper> annos) {
     if (annos.isEmpty()) {
-      annos.add(DEFAULT);
+      annos.add(DEFAULT_WRAPPER);
     }
   }
 
@@ -91,11 +112,11 @@ public class DefaultQualifierFactory implements QualifierFactory {
 
   private static final class NormalQualifier implements Qualifier {
 
-    private final Set<Annotation> annotations;
+    private final Set<AnnotationWrapper> annotations;
     private String identifier;
 
-    private NormalQualifier(final Set<Annotation> annotations) {
-      this.annotations = annotations;
+    private NormalQualifier(final Set<AnnotationWrapper> set) {
+      this.annotations = set;
     }
 
     @Override
@@ -113,8 +134,8 @@ public class DefaultQualifierFactory implements QualifierFactory {
     public String getIdentifierSafeString() {
       if (identifier == null) {
         final StringBuilder builder = new StringBuilder();
-        for (final Annotation anno : annotations) {
-          builder.append(anno.annotationType().getName().replace('.', '_'));
+        for (final AnnotationWrapper wrapper : annotations) {
+          builder.append(wrapper.anno.annotationType().getName().replace('.', '_'));
         }
 
         identifier = builder.toString();
@@ -158,6 +179,35 @@ public class DefaultQualifierFactory implements QualifierFactory {
     @Override
     public String toString() {
       return getIdentifierSafeString();
+    }
+  }
+
+  // TODO handle annotation properties
+  private static final class AnnotationWrapper {
+    private final Annotation anno;
+
+    private AnnotationWrapper(final Annotation anno) {
+      this.anno = anno;
+    }
+
+    @Override
+    public int hashCode() {
+      return anno.annotationType().hashCode();
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+      if (!(obj instanceof AnnotationWrapper)) {
+        return false;
+      }
+      final AnnotationWrapper other = (AnnotationWrapper) obj;
+
+      return anno.annotationType().equals(other.anno.annotationType());
+    }
+
+    @Override
+    public String toString() {
+      return anno.toString();
     }
   }
 
