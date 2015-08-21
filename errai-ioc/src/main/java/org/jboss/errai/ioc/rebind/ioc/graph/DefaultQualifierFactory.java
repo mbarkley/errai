9f2b3568cafcd556a56d742f149bf2d07aa8e628
Max Barkley <mbarkley@redhat.com>
2015-08-21 13:01:49 -0400

    Use sorted set for injector qualifiers (to guarantee consistent naming).

--- errai-ioc/src/main/java/org/jboss/errai/ioc/rebind/ioc/graph/DefaultQualifierFactory.java
+++ errai-ioc/src/main/java/org/jboss/errai/ioc/rebind/ioc/graph/DefaultQualifierFactory.java
@@ -3,9 +3,10 @@ package org.jboss.errai.ioc.rebind.ioc.graph;
 import java.lang.annotation.Annotation;
 import java.util.Collections;
 import java.util.HashMap;
-import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
+import java.util.SortedSet;
+import java.util.TreeSet;
 
 import javax.enterprise.inject.Any;
 import javax.enterprise.inject.Default;
@@ -50,15 +51,17 @@ public class DefaultQualifierFactory implements QualifierFactory {
 
   private static final Qualifier UNIVERSAL = new Universal();
 
-  private final Map<Set<AnnotationWrapper>, NormalQualifier> qualifiers = new HashMap<Set<AnnotationWrapper>, NormalQualifier>();
+  private static final SortedSet<AnnotationWrapper> EMPTY_SORTED_SET = new TreeSet<AnnotationWrapper>();
+
+  private final Map<SortedSet<AnnotationWrapper>, NormalQualifier> qualifiers = new HashMap<SortedSet<AnnotationWrapper>, NormalQualifier>();
 
   {
-    qualifiers.put(Collections.<AnnotationWrapper>emptySet(), new NormalQualifier(Collections.<AnnotationWrapper>emptySet()));
+    qualifiers.put(EMPTY_SORTED_SET, new NormalQualifier(Collections.<AnnotationWrapper>emptySet()));
   }
 
   @Override
   public Qualifier forConcreteInjectable(final HasAnnotations annotated) {
-    final Set<AnnotationWrapper> annos = getQualifierAnnotations(annotated);
+    final SortedSet<AnnotationWrapper> annos = getQualifierAnnotations(annotated);
     annos.add(ANY_WRAPPER);
 
     return getOrCreateQualifier(annos);
@@ -66,12 +69,12 @@ public class DefaultQualifierFactory implements QualifierFactory {
 
   @Override
   public Qualifier forAbstractInjectable(final HasAnnotations annotated) {
-    final Set<AnnotationWrapper> annos = getQualifierAnnotations(annotated);
+    final SortedSet<AnnotationWrapper> annos = getQualifierAnnotations(annotated);
 
     return getOrCreateQualifier(annos);
   }
 
-  private NormalQualifier getOrCreateQualifier(final Set<AnnotationWrapper> annos) {
+  private NormalQualifier getOrCreateQualifier(final SortedSet<AnnotationWrapper> annos) {
     NormalQualifier qualifier = qualifiers.get(annos);
     if (qualifier == null) {
       qualifier = new NormalQualifier(annos);
@@ -81,8 +84,8 @@ public class DefaultQualifierFactory implements QualifierFactory {
     return qualifier;
   }
 
-  private Set<AnnotationWrapper> getQualifierAnnotations(final HasAnnotations annotated) {
-    final Set<AnnotationWrapper> annos = new HashSet<AnnotationWrapper>();
+  private SortedSet<AnnotationWrapper> getQualifierAnnotations(final HasAnnotations annotated) {
+    final SortedSet<AnnotationWrapper> annos = new TreeSet<AnnotationWrapper>();
     for (final Annotation anno : annotated.getAnnotations()) {
       // TODO handle stereotypes
       if (anno.annotationType().isAnnotationPresent(javax.inject.Qualifier.class)) {
@@ -188,7 +191,7 @@ public class DefaultQualifierFactory implements QualifierFactory {
   }
 
   // TODO handle annotation properties
-  private static final class AnnotationWrapper {
+  private static final class AnnotationWrapper implements Comparable<AnnotationWrapper> {
     private final Annotation anno;
 
     private AnnotationWrapper(final Annotation anno) {
@@ -213,6 +216,11 @@ public class DefaultQualifierFactory implements QualifierFactory {
     @Override
     public String toString() {
       return anno.toString();
+    }
+
+    @Override
+    public int compareTo(final AnnotationWrapper o) {
+      return anno.annotationType().getName().compareTo(o.anno.annotationType().getName());
     }
   }
 
