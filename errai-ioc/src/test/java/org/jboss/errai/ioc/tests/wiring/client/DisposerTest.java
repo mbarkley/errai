5cff0fff81e59f6baa878e1259a029b1e48f75f1
Max Barkley <mbarkley@redhat.com>
2015-08-27 13:36:00 -0400

    Add new test for destruction of nested dependent scoped beans.

--- errai-ioc/src/test/java/org/jboss/errai/ioc/tests/wiring/client/DisposerTest.java
+++ errai-ioc/src/test/java/org/jboss/errai/ioc/tests/wiring/client/DisposerTest.java
@@ -4,6 +4,8 @@ import org.jboss.errai.ioc.client.container.IOC;
 import org.jboss.errai.ioc.client.test.AbstractErraiIOCTest;
 import org.jboss.errai.ioc.tests.wiring.client.res.DependentBean;
 import org.jboss.errai.ioc.tests.wiring.client.res.DependentBeanWithDisposer;
+import org.jboss.errai.ioc.tests.wiring.client.res.NestedDependentBean;
+import org.jboss.errai.ioc.tests.wiring.client.res.NestedDependentBeanWIthDisposer;
 import org.jboss.errai.ioc.tests.wiring.client.res.SingletonBeanWithDisposer;
 
 /**
@@ -28,7 +30,6 @@ public class DisposerTest extends AbstractErraiIOCTest {
     assertFalse("inner bean should have been disposed", IOC.getBeanManager().isManaged(innerBean));
     assertTrue("outer bean should not have been disposed", IOC.getBeanManager().isManaged(outerBean));
     assertTrue("bean's destructor should have been called", innerBean.isPreDestroyCalled());
-
   }
 
   public void testDisposerWorksWithDependentScope() {
@@ -44,6 +45,23 @@ public class DisposerTest extends AbstractErraiIOCTest {
     assertFalse("inner bean should have been disposed", IOC.getBeanManager().isManaged(innerBean));
     assertTrue("outer bean should not have been disposed", IOC.getBeanManager().isManaged(outerBean));
     assertTrue("inner bean's destructor should have been called", innerBean.isPreDestroyCalled());
+  }
 
+  public void testDisposerWorksWithNestedDependentScopedBeans() throws Exception {
+
+    final NestedDependentBeanWIthDisposer outerBean = IOC.getBeanManager().lookupBean(NestedDependentBeanWIthDisposer.class).getInstance();
+    assertNotNull(outerBean);
+    final NestedDependentBean middleBean = outerBean.getNestedBean();
+    assertNotNull(middleBean);
+    final DependentBean innerBean = middleBean.getBean();
+    assertNotNull(innerBean);
+
+    outerBean.dispose();
+
+    assertTrue("outer bean should not have been disposed", IOC.getBeanManager().isManaged(outerBean));
+    assertFalse("middle bean should have been disposed", IOC.getBeanManager().isManaged(middleBean));
+    assertTrue("middle bean's destructor should have been called", middleBean.isPreDestroyCalled());
+    assertFalse("inner bean should have been disposed", IOC.getBeanManager().isManaged(innerBean));
+    assertTrue("inner bean's destructor should have been called", innerBean.isPreDestroyCalled());
   }
 }
