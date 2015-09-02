02646a95f8a8b5287aa4aa044100183c1ac0ecc3
Max Barkley <mbarkley@redhat.com>
2015-09-02 16:05:58 -0400

    Fix more weld-integration tests.

--- errai-cdi/weld-integration/src/test/java/org/jboss/errai/cdi/injection/client/PostConstrBeanB.java
+++ errai-cdi/weld-integration/src/test/java/org/jboss/errai/cdi/injection/client/PostConstrBeanB.java
@@ -1,28 +1,21 @@
 package org.jboss.errai.cdi.injection.client;
 
 import javax.annotation.PostConstruct;
-import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
 
 /**
  * @author Mike Brock
  */
-@ApplicationScoped
 public class PostConstrBeanB {
   @Inject
   PostConstrBeanC postConstrBeanC;
 
-  // required to make proxyable
-  public PostConstrBeanB() {
-  }
-
-  @Inject
-  public PostConstrBeanB(PostConstrBeanA selfRefProxy) {
-
-  }
-
   @PostConstruct
   private void postConstr() {
+    // Trigger bean creation since all beans are lazily loaded.
+    postConstrBeanC.noop();
     PostConstructTestUtil.record(PostConstrBeanB.class.getName());
   }
+
+  public void noop() {}
 }
