8457b176ece4b4df94de3463383ed0dd494f814e
Max Barkley <mbarkley@redhat.com>
2015-09-08 16:36:20 -0400

    Fix another test case.

---
+++ errai-cdi/weld-integration/src/test/java/org/jboss/errai/cdi/injection/client/ApplicationScopedBeanB.java
@@ -0,0 +1,8 @@
+package org.jboss.errai.cdi.injection.client;
+
+import javax.enterprise.context.ApplicationScoped;
+
+@ApplicationScoped
+public class ApplicationScopedBeanB implements InterfaceB {
+
+}
