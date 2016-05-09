ae3b8f78250e65e686cbccdd68842316358952e4
Max Barkley <mbarkley@redhat.com>
2016-05-09 17:56:01 -0400

    Progress on dynamic validator implementation.

--- errai-validation/src/main/java/org/jboss/errai/validation/client/dynamic/DynamicValidator.java
+++ errai-validation/src/main/java/org/jboss/errai/validation/client/dynamic/DynamicValidator.java
@@ -1,42 +1,79 @@
+/**
+ * Copyright (C) 2016 Red Hat, Inc. and/or its affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.jboss.errai.validation.client.dynamic;
 
+import java.lang.annotation.Annotation;
+import java.util.HashMap;
 import java.util.Map;
 import java.util.Set;
 
+import javax.enterprise.inject.Alternative;
 import javax.validation.ConstraintViolation;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 /**
- * 
+ *
  * @author Christian Sadilek <csadilek@redhat.com>
+ * @author Max Barkley <mbarkley@redhat.com>
  */
 @SuppressWarnings("unchecked")
+@Alternative
 public class DynamicValidator {
-  
-  // populated at bootstrap time by generated code...
-  Map<DynamicValidatorKey, GeneratedDynamicValidator<?>> validators; 
-  
-  private static DynamicValidator instance  = new DynamicValidator();
-  
-  private DynamicValidator() {};
-  
-  public static DynamicValidator getInstance() {
-    return instance;
+
+  private static final Logger logger = LoggerFactory.getLogger(DynamicValidator.class);
+
+  private final Map<DynamicValidatorKey, GeneratedDynamicValidator<?>> validators = new HashMap<>();
+
+  public void addValidator(final String constraint, final String valueType, final GeneratedDynamicValidator<?> validator) {
+    final DynamicValidatorKey key = new DynamicValidatorKey(constraint, valueType);
+    if (validators.containsKey(key)) {
+      logger.warn("Validator for " + key + " is being overridden.\n\tPrevious: "
+              + validators.get(key).getClass().getSimpleName() + "\n\tNew: " + validator.getClass().getSimpleName());
+    }
+    validators.put(key, validator);
   }
 
-  // not throw NPE, but a reasonable exception if no validator is found for this constraint
-  public <T> Set<ConstraintViolation<T>> validate(String constraint, Map<String, String> parameters, T value) {
-    final GeneratedDynamicValidator<T> dynamicValidator = getValidator(constraint, value);
+  public <T> Set<ConstraintViolation<T>> validate(final Class<? extends Annotation> constraint,
+          final Map<String, Object> parameters, final T value) {
+    return validate(constraint.getName(), parameters, value);
+  }
+
+  public <T> Set<ConstraintViolation<T>> validate(final String constraint, final Map<String, Object> parameters, final T value) {
+    final GeneratedDynamicValidator<T> dynamicValidator = getValidatorOrThrow(constraint, value);
     return dynamicValidator.validate(parameters, value);
   }
 
-  public <T> Set<ConstraintViolation<T>> validate(String constraint, Map<String, String> parameters, T value, String message) {
-    final GeneratedDynamicValidator<T> dynamicValidator = getValidator(constraint, value);
-    return dynamicValidator.validate(parameters, value, message);
-  }
+  private <T> GeneratedDynamicValidator<T> getValidatorOrThrow(final String constraint, final T value) {
+    final DynamicValidatorKey originalKey = new DynamicValidatorKey(constraint, value.getClass().getName());
+    DynamicValidatorKey key = originalKey;
+    GeneratedDynamicValidator<T> dynamicValidator = (GeneratedDynamicValidator<T>) validators.get(key);
 
-  private <T> GeneratedDynamicValidator<T> getValidator(String constraint, T value) {
-    final DynamicValidatorKey key = new DynamicValidatorKey(constraint, value.getClass().getName());
-    final GeneratedDynamicValidator<T> dynamicValidator = (GeneratedDynamicValidator<T>) validators.get(key);
-    return dynamicValidator;
+    while (dynamicValidator == null && key.hasAlias()) {
+      key = key.getAlias().get();
+      dynamicValidator = (GeneratedDynamicValidator<T>) validators.get(key);
+    }
+
+    if (dynamicValidator == null) {
+      throw new IllegalArgumentException("There is no validator for " + originalKey);
+    }
+    else {
+      return dynamicValidator;
+    }
   }
 }
--- errai-validation/src/main/java/org/jboss/errai/validation/client/dynamic/DynamicValidatorKey.java
+++ errai-validation/src/main/java/org/jboss/errai/validation/client/dynamic/DynamicValidatorKey.java
@@ -1,11 +1,68 @@
+/**
+ * Copyright (C) 2016 Red Hat, Inc. and/or its affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.jboss.errai.validation.client.dynamic;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.LinkedHashMap;
+import java.util.LinkedHashSet;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.TreeMap;
+import java.util.TreeSet;
+
+import org.jboss.errai.common.client.function.Optional;
+import org.jboss.errai.common.client.logging.util.StringFormat;
 
 public class DynamicValidatorKey {
 
-  private String constraint;
-  private String valueType;
+  private static final Map<String, String> typeAliases = new HashMap<>();
 
-  public DynamicValidatorKey(String constraint, String valueType) {
+  static {
+    typeAliases.put(Collections.emptyList().getClass().getName(), List.class.getName());
+    typeAliases.put(Collections.singletonList(null).getClass().getName(), List.class.getName());
+    typeAliases.put(ArrayList.class.getName(), List.class.getName());
+    typeAliases.put(LinkedList.class.getName(), List.class.getName());
+
+    typeAliases.put(Collections.emptySet().getClass().getName(), Set.class.getName());
+    typeAliases.put(Collections.singleton(null).getClass().getName(), Set.class.getName());
+    typeAliases.put(HashSet.class.getName(), Set.class.getName());
+    typeAliases.put(LinkedHashSet.class.getName(), Set.class.getName());
+    typeAliases.put(TreeSet.class.getName(), Set.class.getName());
+
+    typeAliases.put(Collections.emptyMap().getClass().getName(), Map.class.getName());
+    typeAliases.put(Collections.singletonMap(null, null).getClass().getName(), Map.class.getName());
+    typeAliases.put(HashMap.class.getName(), Map.class.getName());
+    typeAliases.put(LinkedHashMap.class.getName(), Map.class.getName());
+    typeAliases.put(TreeMap.class.getName(), Map.class.getName());
+
+    typeAliases.put(List.class.getName(), Collection.class.getName());
+    typeAliases.put(Set.class.getName(), Collection.class.getName());
+  }
+
+  private final String constraint;
+  private final String valueType;
+
+  public DynamicValidatorKey(final String constraint, final String valueType) {
     this.constraint = constraint;
     this.valueType = valueType;
   }
@@ -14,16 +71,27 @@ public class DynamicValidatorKey {
     return constraint;
   }
 
-  public void setConstraint(String constraint) {
-    this.constraint = constraint;
-  }
-
   public String getValueType() {
     return valueType;
   }
 
-  public void setValueType(String valueType) {
-    this.valueType = valueType;
+  public boolean hasAlias() {
+    return getAlias().isPresent();
+  }
+
+  public Optional<DynamicValidatorKey> getAlias() {
+    if (isNonPrimitiveArrayType() && !Object[].class.getName().equals(valueType)) {
+      return Optional.ofNullable(new DynamicValidatorKey(constraint, Object[].class.getName()));
+    }
+    else {
+      return Optional
+              .ofNullable(typeAliases.get(valueType))
+              .map(alias -> new DynamicValidatorKey(constraint, alias));
+    }
+  }
+
+  private boolean isNonPrimitiveArrayType() {
+    return valueType.startsWith("[") && valueType.length() > 2;
   }
 
   @Override
@@ -57,6 +125,11 @@ public class DynamicValidatorKey {
     else if (!valueType.equals(other.valueType))
       return false;
     return true;
+  }
+
+  @Override
+  public String toString() {
+    return StringFormat.format("DynamicValidatorKey(constraint=%s,valueType=%s)", constraint, valueType);
   }
 
 }
