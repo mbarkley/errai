0c2392ecb7fd6d6c89034188c682eda18133d41b
Max Barkley <mbarkley@redhat.com>
2016-12-21 17:22:08 -0500

    ERRAI-979: Provide functionality for CSRF protection
    CSRF protection on message bus servlets is enabled by property.
    Enabling the property creates a CSRF token on the first POST
    request to the server bus.
    
    The token can be written to an HTML page as a JavaScript variable
    with a filter, or else the client can acquire it from a challenge
    from the server (a 403 response containing the token as a header).
    
    There is also a filter that protects REST endpoints using the same token.
    When an Errai REST caller finds the token in a global JavaScript variable,
    it will set this as a header for all REST requests.
    
    Errai REST callers will also retry after a challenge from the server
    (403 + token in header).

--- errai-bus/src/main/java/org/jboss/errai/bus/server/servlet/JettyContinuationsServlet.java
+++ errai-bus/src/main/java/org/jboss/errai/bus/server/servlet/JettyContinuationsServlet.java
@@ -41,6 +41,7 @@ import org.slf4j.Logger;
  * using Jetty Continuations.
  */
 public class JettyContinuationsServlet extends AbstractErraiServlet {
+  private static final long serialVersionUID = 1L;
   private static final Logger log = getLogger(JettyContinuationsServlet.class);
   /**
    * Called by the server (via the <tt>service</tt> method) to allow a servlet to handle a GET request by supplying
@@ -86,6 +87,11 @@ public class JettyContinuationsServlet extends AbstractErraiServlet {
 
     session.setAttribute("NoSSE", Boolean.TRUE);
 
+    if (failFromMissingCSRFToken(httpServletRequest)) {
+      prepareTokenChallenge(httpServletRequest, httpServletResponse);
+      return;
+    }
+
     try {
       service.store(createCommandMessage(session, httpServletRequest));
     }
@@ -114,6 +120,9 @@ public class JettyContinuationsServlet extends AbstractErraiServlet {
           case CONNECTING:
           case DISCONNECTING:
             return;
+          case NORMAL:
+          case UNKNOWN:
+            break;
         }
 
         sendDisconnectDueToSessionExpiry(httpServletResponse);
